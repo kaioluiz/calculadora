afc8717cde2d8059d5f4ceddb64b79c895517830
Kaio Oliveira <kaio.oliveira@senior.com.br>
2022-11-23 08:53:17 -0300

    Adicionado evento de click aos botões

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,4 @@
+
 class CalcController {
 
     constructor(){
@@ -7,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     //Será executado após a instância da classe
@@ -18,8 +20,16 @@ class CalcController {
     }
 
     initButtonsEvents(){
-        //pega todas as tags g que esntão dentro da tag com id buttons e dentro da tag com id parts
-        let buttons = document.querySelectorAll("#buttons > g", "#parts > g");
+        //pega todas as tags g que estão dentro da tag com id buttons e dentro da tag com id parts
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        // a partir de dois argumentos nos parâmetros do foreach é necessário coloca-los entre parenteses. 
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', event => {
+                //Comando baseVal se faz necessário por se tratar de svg
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+        });
     }
 
 
